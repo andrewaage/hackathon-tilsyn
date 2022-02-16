3b65a264480ee62d13ee8b3ce9ff9ae87bfea518
andrewaage <andre.rivenaes@pwc.com>
2022-02-16 21:42:39 +0100

    fikset scoring i refmodell

--- score_innleveringer.R
+++ score_innleveringer.R
@@ -1,18 +1,30 @@
 # Kode for å lese inn og score innleveringer
 # Forutsetter at alle innleveringer er csv-filer på likt format
-fasit <- read_csv2("fasit.csv")
+fasit <- read_csv2("data/fasit.csv")
 
-score_innlevering(score, fasit) {
+score_innlevering <- function(score, fasit) {
   fasit %>% 
     left_join(score, by = c("tilsynid")) %>% 
-    mmae(truth = total_karakter, score = score) %>% 
+    mmae(truth = total_karakter, estimate = final_prediction) %>% 
     pull(.estimate)
 }
 
-innleveringer <- fs::dir_map("innleveringer", read_csv2)
+# Lager naiv-innlevering som baseline
+fasit %>%
+  mutate(final_prediction = 0.7) %>% 
+  select(tilsynid, dato, final_prediction) %>% 
+  write_csv2("output/naiv_prediksjon.csv")
 
-scores <- map_dfr(
+files <- fs::dir_ls("output", regexp = "*.csv")
+innleveringer <- map(files, read_csv2)
+
+scores <- map_dbl(
   innleveringer,
   ~ score_innlevering(score = .x, fasit = fasit),
   .id = lag
 )
+
+resultatliste <- tibble(scores = scores, lag = names(scores)) %>% 
+  arrange(scores)
+
+resultatliste
